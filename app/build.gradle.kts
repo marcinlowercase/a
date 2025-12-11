ee97ee9f0ace2d8730eb037f0a1654d1e0b8fa21
Theo <marcinlowercase@gmail.com>
2025-12-11 09:41:07 -0600

    v11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "marcinlowercase.a"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10
-        versionName = "zero[10]"
+        versionCode = 11
+        versionName = "zero[11]"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
