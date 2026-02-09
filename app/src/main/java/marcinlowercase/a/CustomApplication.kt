0274fe1581dd8262a1a3c039c21ffc1183a07d67
Theo <marcinlowercase@gmail.com>
2026-02-09 17:50:53 -0600

    formated

--- app/src/main/java/marcinlowercase/a/CustomApplication.kt
+++ app/src/main/java/marcinlowercase/a/CustomApplication.kt
@@ -7,19 +7,13 @@ import coil.decode.BitmapFactoryDecoder
 import coil.decode.SvgDecoder
 import marcinlowercase.a.core.manager.GeckoManager
 
-// REMOVE the "ImageLoaderFactory" implementation
 class CustomApplication : Application() {
     val geckoManager by lazy { GeckoManager(this) }
 
-
-    // The onCreate() method is the first safe place to access context.
     override fun onCreate() {
-        super.onCreate() // Always call the superclass method first
+        super.onCreate()
 
-
-
-
-
+        // Custom imageLoader to load SVG
         val imageLoader = ImageLoader.Builder(this)
             .components {
                 add(SvgDecoder.Factory())
@@ -27,8 +21,6 @@ class CustomApplication : Application() {
             }
             .crossfade(true)
             .build()
-
-        // Set this as the singleton ImageLoader for the entire app.
         Coil.setImageLoader(imageLoader)
     }
 }
