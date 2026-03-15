1d4bbcccddbdef639c949a24031e8dce1dba86bb
Theo <marcinlowercase@gmail.com>
2026-03-14 20:15:37 -0600

    added license comment on top of files

--- app/src/main/java/marcinlowercase/a/core/service/MediaPlaybackService.kt
+++ app/src/main/java/marcinlowercase/a/core/service/MediaPlaybackService.kt
@@ -1,4 +1,20 @@
-//package marcinlowercase.a.core.service
+///*
+ * Copyright (C) 2026 marcinlowercase
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package marcinlowercase.a.core.service
 //
 //import android.app.*
 //import android.content.Intent
@@ -134,6 +150,22 @@
 //    }
 //}
 
+/*
+ * Copyright (C) 2026 marcinlowercase
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package marcinlowercase.a.core.service
 
 import android.app.*
