a8edf9b77e777904f47fe8d545ad67ddd9e36aaf
Theo <marcinlowercase@gmail.com>
2026-03-31 13:39:50 -0600

    v0.14.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         minSdk = 29 // android 10
         targetSdk = 36
-        versionCode = 35
-        versionName = "0.14.3"
+        versionCode = 36
+        versionName = "0.14.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
