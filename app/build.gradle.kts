6e00d32cdf0ae75461a156f04711bec6b070de92
Theo <marcinlowercase@gmail.com>
2026-07-03 12:39:03 -0600

    v0.21.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         minSdk = 26 // android 8
         targetSdk = 37
-        versionCode = 51
-        versionName = "0.20.2"
+        versionCode = 52
+        versionName = "0.21.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
